e074e0a6586a09bd2f6464e481b56abcde4aec7f
Mark Nair <marknair@me.com>
2023-11-16 16:27:26 -0600

    Initial Commit

---
+++ Lists Project Template/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lists Project Template
+//
+//  Created by Mark Nair on 11/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lists Project Template/Lists_Project_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lists_Project_TemplateApp.swift
+//  Lists Project Template
+//
+//  Created by Mark Nair on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Lists_Project_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
